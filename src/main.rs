37e97709631c2c532018293d4160f5be6babd74c
Patrick Jaberg <patrick.jaberg@student.kit.edu>
2024-01-04 21:33:02 +0100

    hello bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use bevy::prelude::*;
+
+fn hello_world_system() {
+    println!("hello bevy!");
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, hello_world_system)
+        .run();
 }
