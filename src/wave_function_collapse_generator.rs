fa6ad9449174711a840d2dfb855094bb19ade437
David Hedtke <david.hedtke@vescon.com>
2024-01-05 21:37:41 +0100

    Contiuned with WFC

--- src/wave_function_collapse_generator.rs
+++ src/wave_function_collapse_generator.rs
@@ -1,10 +1,40 @@
+use bevy::utils::hashbrown::HashSet;
+
 use crate::multi_vec::*;
 
-#[derive(PartialEq, Eq)]
+enum Direction
+{
+    Up,
+    Left,
+    Down,
+    Right,
+    UpLeft,
+    UpRight,
+    DownLeft,
+    DownRight,
+}
+
+impl From<Direction> for (i32, i32)
+{
+    fn from(value: Direction) -> Self {
+        match value {
+            Direction::Up => (0, -1),
+            Direction::Left => (-1, 0),
+            Direction::Down => (0, 1),
+            Direction::Right => (1, 0),
+            Direction::UpLeft => (-1, -1),
+            Direction::UpRight => (1, -1),
+            Direction::DownLeft => (-1, 1),
+            Direction::DownRight => (1, 1),
+        }
+    }
+}
+
 struct Pattern
 {
-    occurences: usize,
-    definition: Vec<i32>
+    occurrences: usize,
+    flat_definition: Vec<i32>,
+    probability: f32,
 }
 
 fn slice_into_patterns(
@@ -19,8 +49,9 @@ fn slice_into_patterns(
         {
             let mut pattern = Pattern
             {
-                occurences: 0,
-                definition: Vec::new()
+                occurrences: 0,
+                flat_definition: Vec::new(),
+                probability: 0f32,
             };
 
             for y in y_start..(y_start + pattern_size)
@@ -28,35 +59,62 @@ fn slice_into_patterns(
                 for x in x_start..(x_start + pattern_size)
                 {
                     match train_data.get(x, y) {
-                        Some(value) => pattern.definition.push(*value),
+                        Some(value) => pattern.flat_definition.push(*value),
                         None => panic!("Böse :("),
                     }
                 }
             }
 
-            match patterns.iter().position(|old_pattern: &Pattern| *old_pattern == pattern) {
+            match patterns.iter().position(|old_pattern: &Pattern| old_pattern.flat_definition == pattern.flat_definition) {
                 Some(index) =>
                 {
-                    patterns[index].occurences += 1;
+                    patterns[index].occurrences += 1;
+                    
                     continue;
                 },
                 None => { },
             }
 
-            pattern.occurences = 1;
+            pattern.occurrences = 1;
 
             patterns.push(pattern);
         }
     }
 
-    return patterns;
+    let sum_occurrences: usize = patterns
+        .iter()
+        .map(|x| x.occurrences)
+        .sum();
+
+    for pattern in patterns.iter_mut()
+    {
+        pattern.probability = pattern.occurrences as f32 / sum_occurrences as f32;
+    }
+
+    patterns
+}
+
+fn get_valid_directions(x: i32, y: i32, output_length: usize) -> Vec<Direction>
+{
+    match (x, y)
+    {
+        (0, 0) => vec! [ Direction::Right, Direction::Down, Direction::DownRight ],
+        (0, y) if y == output_length as i32 - 1 => vec! [ Direction::Right, Direction::Up, Direction::UpRight ],
+        (0, _) => vec! [ Direction::Right, Direction::Down, Direction::DownRight, Direction::Up, Direction::UpRight ],
+        (x, 0) if x == output_length as i32 -1 => vec! [ Direction::Left, Direction::Down, Direction::DownLeft ],
+        (x, y) if x == output_length as i32 -1 && y == output_length as i32 -1 => vec! [ Direction::Left, Direction::Down, Direction::DownLeft, Direction::Up, Direction::UpLeft ],
+        (x, _) if x == output_length as i32 -1 => vec! [ Direction::Left, Direction::Down, Direction::DownLeft, Direction::Up, Direction::UpLeft ],
+        (_, 0) => vec! [ Direction::Left, Direction::Right, Direction::Down, Direction::DownLeft, Direction::DownRight ],
+        (_, y) if y == output_length as i32 -1 => vec! [ Direction::Left, Direction::Right, Direction::Up, Direction::UpLeft, Direction::UpRight ],
+        (_, _) => vec! [ Direction::Left, Direction::Up, Direction::UpLeft, Direction::UpRight, Direction::Down, Direction::DownLeft, Direction::DownRight ],
+    }
 }
 
 pub fn create_map(
     train_data: MultiVec<i32>,
-    output_width: usize,
-    output_height: usize,
-    pattern_size: usize)
+    output_length: usize,
+    pattern_size: usize,
+    seed: usize)
 {
     let patterns = slice_into_patterns(train_data, pattern_size);
 }
