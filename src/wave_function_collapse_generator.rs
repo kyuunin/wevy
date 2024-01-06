613fcfce294271232e4ba880512826fc6948a6ad
David Hedtke <david.hedtke@vescon.com>
2024-01-06 03:47:12 +0100

    Adding  helping methods to WFC

--- src/wave_function_collapse_generator.rs
+++ src/wave_function_collapse_generator.rs
@@ -69,7 +69,7 @@ struct RulesChecker
 
 impl RulesChecker
 {
-    pub fn new(patterns: Vec<Pattern>) -> Self
+    pub fn new(patterns: &Vec<Pattern>) -> Self
     {
         let mut empty_rules = HashMap::<usize, HashMap<Direction, Vec<usize>>>::new();
 
@@ -225,11 +225,52 @@ fn train_rules(patterns: &Vec<Pattern>, pattern_edge_length: usize, rules_checke
     }
 }
 
+fn initialize_possibilities_for_tiles(output_edge_length: usize, all_pattern_indices: Vec<usize>, possibilites_for_tiles: &mut Vec<Vec<Vec<usize>>>)
+{
+    for y in 0..output_edge_length
+    {
+        let mut possibilites_per_tile: Vec<Vec<usize>> = Vec::new();
+
+        for x in 0..output_edge_length
+        {
+            possibilites_per_tile.push(all_pattern_indices.clone());
+        }
+
+        possibilites_for_tiles.push(possibilites_per_tile);
+    }
+}
+
+fn is_finished(possibilites_for_tiles: &Vec<Vec<Vec<usize>>>) -> bool
+{
+    for row in possibilites_for_tiles
+    {
+        for possibilities_for_one_tile in row
+        {
+            if possibilities_for_one_tile.len() > 1
+            {
+                return false;
+            }
+        }
+    }
+
+    true
+}
+
 pub fn create_map(
     train_data: MultiVec<i32>,
     output_edge_length: usize,
     pattern_edge_length: usize,
     seed: usize)
 {
-    let patterns = slice_into_patterns(train_data, pattern_edge_length);
+    let patterns = &slice_into_patterns(train_data, pattern_edge_length);
+    let pattern_indices = (0..patterns.len()).collect();
+    let rules_checker = &mut RulesChecker::new(patterns);
+    let possibilites_for_tiles: &mut Vec<Vec<Vec<usize>>> = &mut Vec::new();
+
+    train_rules(patterns, pattern_edge_length, rules_checker);
+
+    initialize_possibilities_for_tiles(
+        output_edge_length,
+        pattern_indices,
+        possibilites_for_tiles);
 }
